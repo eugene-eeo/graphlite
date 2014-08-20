231f8363240bcce8636ad1937b073c9e3eae0060
Eugene Eeo <xe0jun@hotmail.com>
2014-08-20 10:20:14 +0800

    bump version and fix bug in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import find_packages
 
 setup(
     name='graphlite',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     description='embedded graph datastore',
 
     author='Eugene Eeo',
     author_email='packwolf58@gmail.com',
     url='https://github.com/eugene-eeo/graphlite',
-    long_description=open('README.rst').read(),
 )
