05826bf680f32a40554a065871beed41148a5835
Eugene Eeo <xe0jun@hotmail.com>
2014-09-05 22:14:33 +0800

    version bump to 1.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='graphlite',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     description='embedded graph datastore',
 
