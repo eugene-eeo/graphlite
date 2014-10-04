3df6c65d8617e96f506a0d57f2bf72235070fd94
Eugene Eeo <xe0jun@hotmail.com>
2014-10-04 19:05:49 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,15 @@ setup(
     author='Eugene Eeo',
     author_email='packwolf58@gmail.com',
     url='https://github.com/eugene-eeo/graphlite',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
