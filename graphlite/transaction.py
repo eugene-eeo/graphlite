f24e526681e716cf84b8653b24499684d0e257c4
Eugene Eeo <packwolf58@gmail.com>
2015-06-25 21:24:22 +0800

    better perform_ops method

--- graphlite/transaction.py
+++ graphlite/transaction.py
@@ -85,7 +85,6 @@ class Transaction(object):
         return bool(self.ops)
 
     def _perform_ops(self, cursor):
-        cursor.execute(SQL.begin)
         for operation, edges in self.ops:
             for edge in edges:
                 cursor.execute(*operation(
@@ -93,7 +92,6 @@ class Transaction(object):
                     rel=edge.rel,
                     dst=edge.dst,
                 ))
-
 
     def perform_ops(self):
         """
@@ -104,6 +102,7 @@ class Transaction(object):
         """
         with self.db:
             with closing(self.db.cursor()) as cursor:
+                cursor.execute(SQL.begin)
                 self._perform_ops(cursor)
 
     def clear(self):
